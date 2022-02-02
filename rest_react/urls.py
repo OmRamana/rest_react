acf4a39a18570763b1af9c7860be4f359cc74c0f
Om <ramanacomfy@gmail.com>
2022-02-02 22:52:34 +0200

    frontend loaded to django

--- rest_react/urls.py
+++ rest_react/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', include('leads.urls')),
-     path('', include('frontend.urls')),
+     path('frontend', include('frontend.urls')),
 ]
